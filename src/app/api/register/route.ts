cc5a2f66f3b2ff28dbd3edcc4e7cfdfb38846cf6
Kevin Xu <kodxity@gmail.com>
2026-01-26 14:51:11 -0500

    Fix ZodError type issue

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -42,7 +42,7 @@ export async function POST(req: Request) {
         return NextResponse.json(userWithoutPassword, { status: 201 });
     } catch (error) {
         if (error instanceof z.ZodError) {
-            return NextResponse.json({ message: error.errors[0].message }, { status: 400 });
+            return NextResponse.json({ message: error.issues[0].message }, { status: 400 });
         }
         return NextResponse.json(
             { message: 'Something went wrong' },
